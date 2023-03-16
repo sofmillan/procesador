5a4d18bd4598aac6c7a8659502545300e08c4a10
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-03-16 17:38:05 -0500

    Fix the errors

--- procesadorNuevo/src/main/java/com/example/procesadorNuevo/model/File.java
+++ procesadorNuevo/src/main/java/com/example/procesadorNuevo/model/File.java
@@ -1,10 +1,10 @@
 package com.example.procesadorNuevo.model;
 
 public class File {
-
     private String path;
     private String type;
 
+    public File(){}
     public File(String path, String type) {
         this.path = path;
         this.type = type;
--- procesadorNuevo/src/main/java/com/example/procesadorNuevo/service/FileProcessor.java
+++ procesadorNuevo/src/main/java/com/example/procesadorNuevo/service/FileProcessor.java
@@ -1,19 +1,22 @@
 package com.example.procesadorNuevo.service;
 
+import com.example.procesadorNuevo.config.RestTemplateClient;
 import com.example.procesadorNuevo.model.People;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 
+@Component
 public class FileProcessor {
 
-    private RestTemplate restTemplate;
+    private RestTemplateClient restTemplate;
 
     @Autowired
-    public FileProcessor(RestTemplate restTemplate) {
+    public FileProcessor(RestTemplateClient restTemplate) {
         this.restTemplate = restTemplate;
     }
 
@@ -21,7 +24,7 @@ public class FileProcessor {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<People> request = new HttpEntity<>(person, headers);
-        boolean response = (this.restTemplate.postForObject("http://localhost:8080/validator/lines", request, Boolean.class));
+        boolean response = (this.restTemplate.postForObject("localhost:8080/validator", request, Boolean.class));
         return response;
     }
 
